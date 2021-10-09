29a43dbee9410423ca277e6b27b6bcd8725c27a4
markusdreyer <markus.dreyer@vipps.no>
2021-10-09 16:22:05 +0200

    Add Accept & revise buttons with no functionality

--- src/components/ActionCards.tsx
+++ src/components/ActionCards.tsx
@@ -1,5 +1,6 @@
 import MaterialTable from "material-table";
-import React from "react";
+import Button from "@mui/material/Button";
+import React, { useState } from "react";
 import { Action } from "../types";
 
 interface Props {
@@ -7,6 +8,7 @@ interface Props {
 }
 
 const ActionCards: React.FC<Props> = (props) => {
+  const [foo, setFoo] = useState<Action>();
   const { models } = props;
   const columns = [
     { title: "Agent", field: "agent" },
@@ -26,6 +28,19 @@ const ActionCards: React.FC<Props> = (props) => {
     showTitle: false,
     toolbar: false,
     paging: false,
+    cellStyle: {
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+      overflow: "hidden",
+      maxWidth: 300,
+    },
+    headerStyle: {
+      width: 100,
+      maxWidth: 100,
+      height: 20,
+      maxHeight: 20,
+    },
+    tableLaoyt: "auto",
   };
 
   const parseActionToTableFormat = (action: Action) => {
@@ -38,6 +53,10 @@ const ActionCards: React.FC<Props> = (props) => {
     ];
   };
 
+  const acceptAction = (action: any) => {
+    setFoo(action);
+  };
+
   return (
     <>
       {models.map((model) => (
@@ -47,12 +66,28 @@ const ActionCards: React.FC<Props> = (props) => {
               <h2>Actions at {i + 1}: </h2>
               <div className="action-card-horizontal-scroll">
                 {time.map((action) => (
-                  <div className="action-card">
+                  <div
+                    className={`action-card ${
+                      foo === action ? "accepted" : ""
+                    }`}
+                  >
                     <MaterialTable
                       options={options}
                       columns={columns}
                       data={parseActionToTableFormat(action)}
                     />
+                    <div className="confirmation-card">
+                      <Button
+                        variant="contained"
+                        color="success"
+                        onClick={() => acceptAction(action)}
+                      >
+                        Accept
+                      </Button>
+                      <Button variant="outlined" color="error">
+                        Revise
+                      </Button>
+                    </div>
                   </div>
                 ))}
               </div>
